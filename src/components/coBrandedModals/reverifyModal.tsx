420564e240e668ece6b27885d91e4a3c07116f4a
wasalimtiaz <wasalimtiaz33@gmail.com>
2023-03-02 13:46:36 +0500

    reverify modal implemented

--- src/components/coBrandedModals/reverifyModal.tsx
+++ src/components/coBrandedModals/reverifyModal.tsx
@@ -1,18 +1,55 @@
-import { FC, useState } from "react";
-import { Input, replaceSubPath } from "components";
+import { FC, useEffect, useState } from "react";
+import { IconLoader, Input, replaceSubPath } from "components";
 import GoogleLogin from "components/authentication/googleLogin";
 import "./coBrandedModals.css";
-import ResetPassModal from "./resetPassModal";
-import { useHistory } from "react-router-dom";
-import { UnAuthRoutes } from "utils";
+import { useHistory, useLocation } from "react-router-dom";
+import { AuthProps, UnAuthRoutes, UnknownType } from "utils";
+import { useConfirmSignUp, useLogin } from "hooks";
+import { validateVerificationCode, withAuth } from "state/auth";
 
-export const ReverifyModal: FC = () => {
+export const ReverifyModal: FC<AuthProps> = ({ getAuth }) => {
   const history = useHistory();
+  const { state } = useLocation();
+  const {
+    performAction,
+    res: { isLoading, success },
+  } = useConfirmSignUp();
+  const {
+    res: { isLoading: loginLoading, success: loginSuccess },
+    performAction: autoLogin,
+  } = useLogin();
 
   const [code, setCode] = useState("");
+  const [codeError, setCodeError] = useState("");
+
+  const validateVerifyForm = (): boolean => {
+    const notValidated = validateVerificationCode(code);
+    if (notValidated) setCodeError("Code length is invalid");
+    return !notValidated;
+  };
 
   const goToLogin = (): void =>
     history.push(replaceSubPath(UnAuthRoutes.SubLogin));
+  const goToVerification = (): void => {
+    if (!loginLoading && !isLoading && validateVerifyForm())
+      performAction(code);
+  };
+
+  useEffect(() => {
+    if (success) {
+      const { email, password } = (state as UnknownType) || {};
+      autoLogin({ email, password });
+    }
+  }, [success]);
+
+  useEffect(() => {
+    const { email, password } = (state as UnknownType) || {};
+    if (!email || !password) history.goBack();
+  }, [state]);
+
+  useEffect(() => {
+    if (loginSuccess) getAuth();
+  }, [loginSuccess]);
 
   return (
     <div className="modal-outer-container">
@@ -21,6 +58,7 @@ export const ReverifyModal: FC = () => {
         <div className="forgot-password-form-fields">
           <Input
             value={code}
+            errorVal={codeError}
             placeholder="Verification Code"
             keyProp=""
             handlers={{
@@ -28,8 +66,9 @@ export const ReverifyModal: FC = () => {
             }}
           />
         </div>
-        <div className="modal-button">
+        <div className="modal-button" onClick={goToVerification}>
           <span className="modal-button-text">Verify</span>
+          {isLoading && <IconLoader />}
         </div>
 
         <div className="google-login forgot-password-modal">
@@ -43,4 +82,4 @@ export const ReverifyModal: FC = () => {
   );
 };
 
-export default ResetPassModal;
+export default withAuth(ReverifyModal);
