27c7ea4878d25ae6e70b1f8236d1b2520eb208a7
wasalimtiaz <wasalimtiaz33@gmail.com>
2023-03-02 14:16:51 +0500

    subdomain check added with 2 letter hostname and 8 letter id

--- src/pages/coBranded/main.tsx
+++ src/pages/coBranded/main.tsx
@@ -1,14 +1,21 @@
 import { replaceSubPath } from "components";
-import { FC } from "react";
-import { useHistory } from "react-router-dom";
+import { FC, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import { UnAuthRoutes } from "utils";
 import "./styles.css";
 
 export const CoBrandedMainPage: FC = () => {
   const history = useHistory();
+  const { id } = useParams<{ id: string }>();
 
   const goToLogin = (): void =>
     history.push(replaceSubPath(UnAuthRoutes.SubLogin));
+
+  useEffect(() => {
+    const initialHost = window.location.hostname.split(".")[0];
+    if (initialHost.length !== 2 || !id || id.length !== 8)
+      history.push(UnAuthRoutes.Login);
+  }, []);
 
   return (
     <div className="cobranded-container">
