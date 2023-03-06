cdf8687479bf31bd0028bd77f93a9d5ba89c51a4
wasalimtiaz <wasalimtiaz33@gmail.com>
2023-03-06 17:22:26 +0500

    landing page design updated

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,12 +2,28 @@ import React from "react";
 import "./style.css";
 
 export const LandingPage: React.FC = () => {
+  const openContactUs = (): void => {
+    window.location.href = "mailto:hello@edcsquared.io";
+  };
   return (
     <div className="landing-page-wrapper">
       <div className="landing-page-bg_image">
         <img src="/images/bg-landing.png" alt="cover-image" />
       </div>
-      <div className="landing-page-menu-header">
+      <div className="landing-page-container">
+        <div className="landing-page-navbar comingSoon">
+          <div className="landing-page-logo">
+            <img src="/images/edc-logo.png" alt="edc-squared" />
+          </div>
+          <div className="landing-page-menu-header">
+            <div className="landing-page-menu">
+              <div className="landing-page-menu-items" onClick={openContactUs}>
+                Contact us
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="landing-page-menu-header">
         <div className="landing-page-menu">
           <div className="landing-page-menu-items">For Creators</div>
           <div className="landing-page-menu-items">For Brands</div>
@@ -27,41 +43,45 @@ export const LandingPage: React.FC = () => {
             <img src="/images/instagram.png" alt="instagram-icon" />
           </div>
         </div>
-      </div>
-      <div className="landing-page-body">
-        <div className="landing-page-title">
-          Everyday creators, everyday creative.
-        </div>
-        <div className="landing-page-subtitle">Driving change and impact.</div>
-        <div className="landing-page-workBtn">How it works</div>
-        <div className="landing-page-steps-wrapper">
-          <div className="landing-page-single-steps">
-            <div className="landing-page-step-number-circle">
-              <div className="landing-page-step-number-text">1</div>
-            </div>
-            <div className="landing-page-step-text">
-              Creators post their content about a brand or topic and shares it
-              with the brand.
-            </div>
+      </div> */}
+        <div className="landing-page-body">
+          <div className="landing-page-title">
+            Everyday creators, everyday creative.
           </div>
-          <div className="landing-page-single-steps">
-            <div className="landing-page-step-number-circle">
-              <div className="landing-page-step-number-text">2</div>
-            </div>
-            <div className="landing-page-step-text">
-              Brands reviews the content, if they like the content, they promote
-              it.
-            </div>
+          <div className="landing-page-subtitle">
+            Driving change and impact.
           </div>
-          <div className="landing-page-single-steps">
-            <div className="landing-page-step-number-circle">
-              <div className="landing-page-step-number-text">3</div>
+          {/* <div className="landing-page-workBtn">How it works</div> */}
+          <div className="landing-page-workBtn">Coming Soon</div>
+          {/* <div className="landing-page-steps-wrapper">
+            <div className="landing-page-single-steps">
+              <div className="landing-page-step-number-circle">
+                <div className="landing-page-step-number-text">1</div>
+              </div>
+              <div className="landing-page-step-text">
+                Creators post their content about a brand or topic and shares it
+                with the brand.
+              </div>
             </div>
-            <div className="landing-page-step-text">
-              Creators earn money for their content. Brands get authentic
-              content at scale.
+            <div className="landing-page-single-steps">
+              <div className="landing-page-step-number-circle">
+                <div className="landing-page-step-number-text">2</div>
+              </div>
+              <div className="landing-page-step-text">
+                Brands reviews the content, if they like the content, they
+                promote it.
+              </div>
             </div>
-          </div>
+            <div className="landing-page-single-steps">
+              <div className="landing-page-step-number-circle">
+                <div className="landing-page-step-number-text">3</div>
+              </div>
+              <div className="landing-page-step-text">
+                Creators earn money for their content. Brands get authentic
+                content at scale.
+              </div>
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
