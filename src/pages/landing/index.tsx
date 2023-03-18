10aa19eeb0815d1b288c50ac2338ab4471817e1e
Wasal Imtiaz <wasalimtiaz33@gmail.com>
2023-03-18 12:07:50 -0700

    Home page revamped

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,65 +2,91 @@ import React from "react";
 import "./style.css";
 
 export const LandingPage: React.FC = () => {
-  const openContactUs = (): void => {
-    window.location.href = "mailto:hello@edcsquared.io";
-  };
   return (
     <div className="landing-page-wrapper">
-      <div className="landing-page-bg_image">
-        <img src="/images/bg-landing.png" alt="cover-image" />
-      </div>
-      <div className="landing-page-container">
-        <div className="landing-page-navbar comingSoon">
-          <div className="landing-page-logo">
-            <img src="/images/edc-logo.svg" alt="edc-squared" />
-          </div>
-          <div className="landing-page-menu-header">
-            <div className="landing-page-menu">
-              <div className="landing-page-menu-items" onClick={openContactUs}>
-                Contact us
+      <div className="landing-content-container-gradient">
+        <div className="landing-page-container">
+          <div className="landing-page-navbar comingSoon">
+            <div className="landing-page-logo">
+              <img
+                src="/images/edc-squared-landing-logo.svg"
+                alt="edc-squared"
+              />
+            </div>
+            <div className="landing-page-menu-header">
+              <div className="landing-page-menu">
+                <div className="landing-page-menu-items">Home</div>
+                <div className="landing-page-menu-items">For Creators</div>
+                <div className="landing-page-menu-items">For Brands</div>
+                <div className="landing-page-menu-items">Say Hello</div>
+                <div className="login-signup">Login / Sign up</div>
+
+                <div className="landing-page-socials">
+                  <div className="landing-page-socials-item">
+                    <img src="/images/linkedin.png" alt="linkedin-icon" />
+                  </div>
+                  <div className="landing-page-socials-item">
+                    <img src="/images/tiktok.png" alt="tiktok-icon" />
+                  </div>
+                  <div className="landing-page-socials-item">
+                    <img src="/images/instagram.png" alt="instagram-icon" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* <div className="landing-page-menu-header">
-        <div className="landing-page-menu">
-          <div className="landing-page-menu-items">For Creators</div>
-          <div className="landing-page-menu-items">For Brands</div>
-          <div className="landing-page-menu-items">Contact us</div>
-          <div className="landing-page-menu-items btn-item">
-            Login / Sign Up
+          <div className="landing-content-container">
+            <div className="landing-page-body">
+              <div className="landing-page-title">
+                Everyday Creators, everyday Creative.
+              </div>
+              <div className="landing-page-subtitle">
+                Your content, your story, your impact.
+              </div>
+
+              <div className="landing-page-detailed-content-container">
+                <div className="landing-page-detailed-content">
+                  We are passionate about creating positive social impact by
+                  empowering anyone across the globe to become a content
+                  creator..
+                </div>
+                <div className="landing-page-detailed-content">
+                  One of the major challenges for brands is having to drive
+                  influence and impact amongst their audience. An audience who
+                  have an ever-decreasing attention span. Creating authentic
+                  content at scale solves this challenge, but, until now has not
+                  been a viable solution.
+                </div>
+                <div className="landing-page-detailed-content">
+                  EDC squared is focused on solving this challenge by enabling
+                  everyday creators and seamlessly connecting them with brands.
+                  We create opportunities for individuals whilst solving major
+                  industry challenges for brands.
+                </div>
+              </div>
+
+              <div className="for-landing-content-btn-container">
+                <div className="for-brands-btn">
+                  <span className="for-content-btn-text">FOR BRANDS</span>
+                </div>
+                <div className="for-creators-btn">
+                  <span className="for-content-btn-text">FOR CREATORS</span>
+                </div>
+              </div>
+            </div>
+            <img src="/images/landing-union.svg" />
           </div>
         </div>
-        <div className="landing-page-socials">
-          <div className="landing-page-socials-item">
-            <img src="/images/linkedin.png" alt="linkedin-icon" />
-          </div>
-          <div className="landing-page-socials-item">
-            <img src="/images/tiktok.png" alt="tiktok-icon" />
-          </div>
-          <div className="landing-page-socials-item">
-            <img src="/images/instagram.png" alt="instagram-icon" />
-          </div>
-        </div>
-      </div> */}
-        <div className="landing-page-body">
-          <div className="landing-page-title">
-            Everyday creators, everyday creative.
-          </div>
-          <div className="landing-page-subtitle">
-            Your content, your story, your impact.
-          </div>
-          {/* <div className="landing-page-workBtn">How it works</div> */}
-          <div className="landing-page-workBtn">Coming Soon</div>
-          {/* <div className="landing-page-steps-wrapper">
+
+        <div className="how-it-works-container">
+          <div className="landing-page-workBtn">How it works</div>
+          <div className="landing-page-steps-wrapper">
             <div className="landing-page-single-steps">
               <div className="landing-page-step-number-circle">
                 <div className="landing-page-step-number-text">1</div>
               </div>
               <div className="landing-page-step-text">
-                Creators post their content about a brand or topic and shares it
-                with the brand.
+                Creators post content about the brands and topics they love.
               </div>
             </div>
             <div className="landing-page-single-steps">
@@ -68,8 +94,7 @@ export const LandingPage: React.FC = () => {
                 <div className="landing-page-step-number-text">2</div>
               </div>
               <div className="landing-page-step-text">
-                Brands reviews the content, if they like the content, they
-                promote it.
+                Content is linked to brands via EDC squared
               </div>
             </div>
             <div className="landing-page-single-steps">
@@ -77,11 +102,38 @@ export const LandingPage: React.FC = () => {
                 <div className="landing-page-step-number-text">3</div>
               </div>
               <div className="landing-page-step-text">
-                Creators earn money for their content. Brands get authentic
-                content at scale.
+                If brands like it, they promote it.
               </div>
             </div>
-          </div> */}
+            <div className="landing-page-single-steps">
+              <div className="landing-page-step-number-circle">
+                <div className="landing-page-step-number-text">4</div>
+              </div>
+              <div className="landing-page-step-text">
+                Creators earn from their content. Brands get authentic content
+                at scale.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="landing-footer">
+          <div className="landing-footer-text-container">
+            <div className="landing-footer-text">Home</div>
+            <div className="landing-footer-text">For Creators</div>
+            <div className="landing-footer-text">For Brands</div>
+            <div className="landing-footer-text">Say Hello</div>
+          </div>
+
+          <div className="landing-footer-img-container">
+            <img src="/images/landing-insta.svg" />
+            <img src="/images/landing-twitter.svg" />
+            <img src="/images/landing-tiktok.svg" />
+          </div>
+
+          <div className="landing-footer-text">
+            © 2023 Copyright EDC Squared. All Rights Reserved.
+          </div>
         </div>
       </div>
     </div>
