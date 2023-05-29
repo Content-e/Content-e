9ca4b70706282544aeb81c1b6a66f6557a8a3efc
Anton Dubik <anton.dubik33@gmail.com>
2023-05-29 13:35:56 +0200

    fix: use Footer component on Sign Up page

--- src/components/authentication/register.tsx
+++ src/components/authentication/register.tsx
@@ -3,8 +3,6 @@ import { useState, FC, useMemo } from "react";
 import { defaultSignUpError, defaultSignUpState } from "utils";
 import { useSignup } from "hooks";
 import { validateEmail, validateFullName, withAuth } from "state/auth";
-import { UnAuthRoutes } from "utils";
-import { useHistory } from "react-router-dom";
 import "./styles/login.scss";
 // import GoogleLogin from "./googleLogin";
 import { USER_TYPES } from "API";
@@ -18,12 +16,11 @@ import { IconLoader } from "components/loader";
 import Modal from "./modal";
 import HeaderDesktop from "components/authentication/components/header-desktop";
 import HeaderMobile from "components/authentication/components/header-mobile";
+import Footer from "./components/footer";
 
 export const Register: FC = () => {
   const [creator, setCreator] = useState<boolean | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const path = window.location.pathname;
-  const history = useHistory();
 
   const {
     performAction,
@@ -187,70 +184,8 @@ export const Register: FC = () => {
           <img src="/images/login-image.png" />
         </div>
       </div>
-      <div className="landing-footer">
-        <div className="landing-footer-text-container">
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Landing)}
-          >
-            Home
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Creators)}
-          >
-            For Creators
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Brands)}
-          >
-            For Brands
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.SayHello)}
-          >
-            Say Hello
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.SayHello)}
-          >
-            Say Hello
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() =>
-              path === "/homePageLogin"
-                ? history.push(UnAuthRoutes.Register)
-                : history.push(UnAuthRoutes.Login)
-            }
-          >
-            Login / Sign up
-          </div>
-        </div>
-
-        <div className="landing-footer-img-container">
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/company/edcsquared/"
-          >
-            <img src="/images/landing-linkedin.svg" />
-          </a>
-          <a target="_blank" href="https://www.instagram.com/edcsq/">
-            <img src="/images/landing-insta.svg" />
-          </a>
-          <a target="_blank" href="https://www.tiktok.com/@edcsquared">
-            <img src="/images/landing-tiktok.svg" />
-          </a>
-        </div>
-
-        <div className="landing-footer-text">
-          © 2023 Copyright EDC Squared. All Rights Reserved.
-        </div>
-        <Modal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)} />
-      </div>
+      <Footer />
+      <Modal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)} />
     </div>
   );
 };
