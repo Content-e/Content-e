fa71f0ffb7b5329be8ad06d658daaa854c7c418b
Anton Dubik <anton.dubik33@gmail.com>
2023-05-29 12:35:50 +0200

    feat: isolate 'Footer' on login related pages

--- src/components/authentication/forgetPass.tsx
+++ src/components/authentication/forgetPass.tsx
@@ -6,6 +6,7 @@ import { useForgetPass } from "hooks";
 import { validateEmail, withAuth } from "state/auth";
 import HeaderDesktop from "./components/header-desktop";
 import HeaderMobile from "./components/header-mobile";
+import Footer from "./components/footer";
 
 export const ForgetPassword: FC = () => {
   const history = useHistory();
@@ -16,8 +17,6 @@ export const ForgetPassword: FC = () => {
 
   const [email, setEmail] = useState<string>("");
   const [emailError, setEmailError] = useState<string | null>(null);
-
-  const path = window.location.pathname;
 
   const updateState = (_: string, value: string): void => {
     setEmail(value);
@@ -74,69 +73,7 @@ export const ForgetPassword: FC = () => {
           <img src="/images/login-image.png" alt="picture of a landscape" />
         </div>
       </div>
-      <div className="landing-footer">
-        <div className="landing-footer-text-container">
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Landing)}
-          >
-            Home
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Creators)}
-          >
-            For Creators
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.Brands)}
-          >
-            For Brands
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.SayHello)}
-          >
-            Say Hello
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() => history.push(UnAuthRoutes.SayHello)}
-          >
-            Say Hello
-          </div>
-          <div
-            className="landing-footer-text"
-            onClick={() =>
-              path === "/homePageLogin"
-                ? history.push(UnAuthRoutes.Register)
-                : history.push(UnAuthRoutes.Login)
-            }
-          >
-            Login / Sign up
-          </div>
-        </div>
-
-        <div className="landing-footer-img-container">
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/company/edcsquared/"
-          >
-            <img src="/images/landing-linkedin.svg" />
-          </a>
-          <a target="_blank" href="https://www.instagram.com/edcsq/">
-            <img src="/images/landing-insta.svg" />
-          </a>
-          <a target="_blank" href="https://www.tiktok.com/@edcsquared">
-            <img src="/images/landing-tiktok.svg" />
-          </a>
-        </div>
-
-        <div className="landing-footer-text">
-          © 2023 Copyright EDC Squared. All Rights Reserved.
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 };
