0b1ecb93937ceaaf42a9e548d679cf677d854e30
eug-vs <eugene@eug-vs.xyz>
2023-05-31 19:31:14 +0300

    fix: resolve eslint issues
    Please make sure you resolve eslint locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Amplify } from "aws-amplify";
 import "bootstrap/dist/css/bootstrap.min.css";
 import config from "aws-exports";
