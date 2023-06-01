d7c59c8bf0600d5bd1c14bf186fb7cd6256e5c31
eug-vs <eugene@eug-vs.xyz>
2023-06-02 01:42:14 +0300

    chore: add !important to tailwind classes
    This is needed because bootstrap has !important on its utility
    classes and they overlap with tailwind
    
    Killing it with the same weapon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  // This is a hack around bootstrap utility classes having !important, resulting in some overlapping styles resolving in favor of bootstrap
+  // TODO: remove bootstrap and this line
+  important: true,
   theme: {
     extend: {
       fontFamily: {
