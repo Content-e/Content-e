689cb50de31c0a72ca640cf1730a02d16743e677
eug-vs <eugene@eug-vs.xyz>
2023-06-02 20:31:03 +0300

    fix: remove bullshit text from brand description

--- src/components/brandDescription/brandDescription.tsx
+++ src/components/brandDescription/brandDescription.tsx
@@ -30,21 +30,6 @@ export const BrandDesciption: FC<Props & ProfileProps> = ({
     if (!videoUrls) return [];
     return videoUrls.filter((e) => e?.length) as Array<string>;
   }, [videoUrls]);
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-  const [mobileDetailsOpened, setMobileDetailsOpened] = useState(false);
-  //const [modalOpened, setModalOpened] = useState(false);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
 
   return (
     <div className="paper w-full min-h-[60vh] flex flex-col justify-between">
@@ -52,85 +37,7 @@ export const BrandDesciption: FC<Props & ProfileProps> = ({
         <div className="text-xl text-primary font-bold">
           Campaign brief details
         </div>
-        <div className="creator-dashboard__text-wrapper">
-          <div className="creator-dashboard__text dark short-gap">
-            <p>{detail}</p>
-            {windowSize <= 991 && mobileDetailsOpened ? (
-              <>
-                <p>
-                  One of the most important aspects of a campaign brief is a
-                  clearly defined target audience. It's essential that a
-                  creative team or third-party agency understand who they're
-                  trying to reach to encourage them to take action. Depending on
-                  the amount of data you hold about your audience, you may have
-                  detailed personas or primary and secondary audiences. Provide
-                  as much detail as possible, enabling your team to gain an
-                  in-depth understanding of your target audience.
-                </p>
-                <p>
-                  While your creative team may be familiar with the product or
-                  products that are the campaign focus, the campaign brief
-                  provides an opportunity to outline aspects you wish to
-                  highlight. Explain what problem your product solves for
-                  consumers. If your product is the best, most efficient or the
-                  first of its kind, mention this, and ensure that you can
-                  substantiate any claims that you make.
-                </p>
-                <p>
-                  Include relevant context about competitors and the problem in
-                  the market that your product solves. Commercial context helps
-                  marketing teams and agencies suggest.
-                </p>
-              </>
-            ) : null}
-          </div>
-          <div className="creator-dashboard__creative-learn">
-            <button
-              className={`${mobileDetailsOpened ? "opened" : ""}learn-more`}
-              onClick={() => setMobileDetailsOpened((prev) => !prev)}
-            >
-              Learn more
-            </button>
-          </div>
-          {windowSize <= 991 && mobileDetailsOpened ? (
-            <div className="creator-dashboard__text dark short-gap">
-              <p>
-                The budget is crucial information as it lets creative teams
-                assess the scale that the project can reach and ensures that
-                they pitch ideas within the company's financial resources. It's
-                inadvisable to allow marketing teams to use the budget as they
-                wish because projects always have a specific fiscal limit. Teams
-                can use their time and resources efficiently if they understand
-                the budgetary constraints of the campaign.
-              </p>
-              <p>
-                If the company already has an established brand, share
-                information about its tone and image to ensure that the campaign
-                is consistent with it. You may have a brand guideline or style
-                guide that you can share. Describe the tone of voice for
-                previous marketing campaigns and communications to ensure that
-                the team's work aligns with the established company image.
-              </p>
-              <p>
-                If you have regular marketing channels that you use
-                consistently, you can mention them in the brief. For example, if
-                you always align social media content with current campaigns,
-                you may specify this so the creative team can provide content
-                that also works for social media. The creative team's ideas
-                might be more suitable for other channels, so being transparent
-                about your usual channels can help to guide the project.
-              </p>
-              <p>
-                Companies with extensive marketing experience may have
-                information about the success of previous campaigns. Provide
-                creative teams with this information, including the campaign
-                objectives and performance. Understanding the success or failure
-                of historic marketing projects can help creative teams identify
-                effective marketing methods for the target audience.
-              </p>
-            </div>
-          ) : null}
-        </div>
+        <p className="text-gray-500 prose my-8">{detail}</p>
       </section>
       <div className="w-full flex justify-center">
         {authenticatedUrls.length > 0 && (
