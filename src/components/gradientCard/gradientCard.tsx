073704d97aa4b221fda096cae3dfe19a047d4469
eug-vs <eugene@eug-vs.xyz>
2023-06-06 15:44:31 +0300

    feat: enlarge inner card in gradient card

--- src/components/gradientCard/gradientCard.tsx
+++ src/components/gradientCard/gradientCard.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function GradientCard({ children }: PropsWithChildren) {
   return (
-    <div className="gradient rounded-2xl text-white p-5 flex items-end relative overflow-hidden">
+    <div className="gradient rounded-2xl text-white p-5 flex relative overflow-hidden">
       <div className="absolute bg-glass bg-cover inset-0 mix-blend-difference" />
       <div className="gradient rounded-2xl p-3 w-full relative overflow-hidden">
         <div className="absolute bg-glass bg-cover inset-0 mix-blend-color-dodge" />
