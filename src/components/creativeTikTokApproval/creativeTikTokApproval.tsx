ef0b0bf0240aaeae5d49826bc323939d882f8396
eug-vs <eugene@eug-vs.xyz>
2023-06-06 19:35:40 +0300

    feat: inline autoplayed video in details

--- src/components/creativeTikTokApproval/creativeTikTokApproval.tsx
+++ src/components/creativeTikTokApproval/creativeTikTokApproval.tsx
@@ -2,10 +2,10 @@ import { FC, useEffect, useState } from 'react';
 import { CreativeRequestStatus, UnknownType } from 'utils';
 import { CreativeRequest } from 'API';
 import { ViewRequestProps, withRequestView } from 'state/requests';
-import CreativeTikTokVideo from './creativeTikTokVideo';
 import { Storage } from 'aws-amplify';
 import Button from 'components/ui/button';
 import Modal from 'components/ui/modal';
+import Status from 'components/ui/status';
 
 interface Props {
   request?: CreativeRequest | null;
@@ -25,7 +25,6 @@ export const CreativeTikTokApproval: FC<Props & ViewRequestProps> = ({
   errorMsg,
   tiktokVideo,
 }) => {
-  const openedSidebar = false;
   const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
   const [awsURL, setAwsURL] = useState<string>('');
 
@@ -84,70 +83,41 @@ export const CreativeTikTokApproval: FC<Props & ViewRequestProps> = ({
 
       <section className="paper">
         <h1 className="text-lg text-primary font-bold">Creative</h1>
-        <div
-          className={`brand-dashboard__slider
-            ${openedSidebar ? 'short-slider' : ''}
-          `}
-        >
-          <div className="brand-dashboard__slider-left">
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M16.1568 5.69757C16.4894 6.00267 16.4894 6.49733 16.1568 6.80243L10.5469 11.9476C10.2142
-                12.2527 10.2142 12.7473 10.5469 13.0524L16.1568 18.1976C16.4894 18.5027 16.4894 18.9973
-                16.1568 19.3024C15.8241 19.6075 15.2848 19.6075 14.9521 19.3024L9.34222 14.1573C8.34426
-                13.242 8.34426 11.758 9.34222 10.8427L14.9521 5.69757C15.2848 5.39248 15.8241 5.39248
-                16.1568 5.69757Z"
-                fill="#005F73"
-              />
-            </svg>
-          </div>
-          <div className="brand-dashboard__slider-right">
-            <svg
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8.84324 19.3024C8.51059 18.9973 8.51059 18.5027 8.84324 18.1976L14.4531 13.0524C14.7858
-                12.7473 14.7858 12.2527 14.4531 11.9476L8.84324 6.80243C8.51059 6.49733 8.51059 6.00267
-                8.84324 5.69757C9.1759 5.39248 9.71524 5.39248 10.0479 5.69757L15.6578 10.8427C16.6557 11.758
-                16.6557 13.242 15.6578 14.1573L10.0479 19.3024C9.71524 19.6075 9.1759 19.6075 8.84324
-                19.3024Z"
-                fill="#005F73"
-              />
-            </svg>
-          </div>
-          <div className="brand-dashboard__slider-items">
-            <div className="brand-dashboard__slider-item left-slider-item"></div>
-            <CreativeTikTokVideo awsURL={awsURL} {...tiktokVideo} />
-            <div className="brand-dashboard__slider-item right-slider-item"></div>
-          </div>
-          {request?.status === CreativeRequestStatus.New && (
-            <div className="brand-dashboard__slider-buttons">
-              <button onClick={onApprove}>approve</button>
-              <button onClick={onReject}>reject</button>
-            </div>
+        <div className="m-2 flex justify-center items-center">
+          {loading && 'LOADING'}
+          {awsURL ? (
+            <video controls className="outline-none" autoFocus autoPlay muted>
+              <source src={awsURL} />
+            </video>
+          ) : tiktokVideo?.videoUrl ? (
+            <iframe
+              className="creative-video-iframe"
+              src={tiktokVideo?.videoUrl}
+              name={`video-${tiktokVideo?.videoUrl}`}
+              // eslint-disable-next-line max-len
+              sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-top-navigation allow-same-origin"
+            />
+          ) : (
+            <p className="prose text-danger font-bold">
+              There was an error fetching TikTok video{' '}
+              {errorMsg && `: ${errorMsg}`}
+            </p>
           )}
         </div>
         <div className="flex justify-center mt-6">
-          <div className="flex gap-4">
-            <Button onClick={onApprove}>Approve</Button>
-            <Button onClick={onReject} variant="secondary">
-              Reject
-            </Button>
-          </div>
+          {request?.status === CreativeRequestStatus.New ? (
+            <div className="flex gap-4">
+              <Button onClick={onApprove}>Approve</Button>
+              <Button onClick={onReject} variant="secondary">
+                Reject
+              </Button>
+            </div>
+          ) : (
+            <p>
+              This request is in status:{' '}
+              <Status value={request?.status || ''} />
+            </p>
+          )}
         </div>
       </section>
     </>
