fe1b6159216c4e422f4f09f922ab62dc8848d02c
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-06-09 13:40:09 +0300

    feat: add GradientCard to wallet

--- src/components/walletCard/walletCard.tsx
+++ src/components/walletCard/walletCard.tsx
@@ -33,17 +33,14 @@ export default function WalletCard({ dueDateCheck, walletTitle, cost }: Props) {
   }
 
   return (
-    <div className="wallet-container">
-      <div className="creator-dashboard__bg long-glass-bg"></div>
-      <div className="wallet-card-block">
-        <div className="wallet-header">{walletTitle}</div>
-        {dueDateCheck && (
-          <div className="wallet-due-date">
-            Next payment processing date is {monthName}, {day}.
-          </div>
-        )}
-        <div className="wallet-cost">{cost}</div>
-      </div>
-    </div>
+    <>
+      <div className="wallet-header">{walletTitle}</div>
+      {dueDateCheck && (
+        <div className="wallet-due-date">
+          Next payment processing date is {monthName}, {day}.
+        </div>
+      )}
+      <div className="wallet-cost">{cost}</div>
+    </>
   );
 }
--- src/pages/wallet/wallet.tsx
+++ src/pages/wallet/wallet.tsx
@@ -2,6 +2,7 @@ import { UserProfile } from 'API';
 import PaymentCard from 'components/paymentCard/paymentCard';
 import PaymentDetails from 'components/paymentCard/paymentDetails';
 import WalletCard from 'components/walletCard/walletCard';
+import GradientCard from 'components/gradientCard/gradientCard';
 import './wallet.css';
 
 interface Props {
@@ -11,17 +12,21 @@ interface Props {
 export default function Wallet({ data }: Props) {
   return (
     <>
-      <div className="wallet-section">
-        <WalletCard
-          dueDateCheck={true}
-          walletTitle="EDC Wallet"
-          cost={`$${data.userWallet ? data.userWallet.currentBalance : '0'}`}
-        />
-        <WalletCard
-          dueDateCheck={false}
-          walletTitle="Total earned"
-          cost={`$${data.userWallet ? data.userWallet.totalEarned : '0'}`}
-        />
+      <div className="grid xl:grid-cols-2 grid-cols-1 gap-8 w-full mb-8">
+        <GradientCard>
+          <WalletCard
+            dueDateCheck={true}
+            walletTitle="EDC Wallet"
+            cost={`$${data.userWallet ? data.userWallet.currentBalance : '0'}`}
+          />
+        </GradientCard>
+        <GradientCard>
+          <WalletCard
+            dueDateCheck={false}
+            walletTitle="Total earned"
+            cost={`$${data.userWallet ? data.userWallet.totalEarned : '0'}`}
+          />
+        </GradientCard>
       </div>
       <div className="wallet-section">
         <PaymentCard />
