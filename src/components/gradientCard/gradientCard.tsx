e441bcc70bef525df3735af98b0b229cd59496c2
maksimvasilevskiy <97239271+maksimvasilevskiy@users.noreply.github.com>
2023-06-09 16:25:08 +0300

    fix: remove dark side of gradient cards

--- src/components/gradientCard/gradientCard.tsx
+++ src/components/gradientCard/gradientCard.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 export default function GradientCard({ children }: PropsWithChildren) {
   return (
     <div className="gradient rounded-2xl text-white p-5 flex relative overflow-hidden">
-      <div className="absolute bg-glass bg-cover inset-0 mix-blend-difference" />
+      <div className="absolute bg-glass bg-cover inset-0 mix-blend-difference opacity-50" />
       <div className="gradient rounded-2xl p-3 w-full relative overflow-hidden">
         <div className="absolute bg-glass bg-cover inset-0 mix-blend-color-dodge" />
         {children}
