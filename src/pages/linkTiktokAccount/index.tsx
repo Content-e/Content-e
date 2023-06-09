ea7439b3daadda4783952cf4a7a00bcb40684337
eug-vs <eugene@eug-vs.xyz>
2023-06-09 14:31:13 +0300

    feat: add modal after tiktok link flow

--- src/pages/linkTiktokAccount/index.tsx
+++ src/pages/linkTiktokAccount/index.tsx
@@ -5,6 +5,8 @@ import { useHistory } from 'react-router-dom';
 import { withProfile } from 'state/profileSteps';
 import { useLinkTiktokAccount } from 'hooks';
 import './style.css';
+import Modal from 'components/ui/modal';
+import Button from 'components/ui/button';
 
 const LinkTiktokAccount: React.FC<ProfileProps> = ({
   profileState: { data },
@@ -14,25 +16,54 @@ const LinkTiktokAccount: React.FC<ProfileProps> = ({
     linkTiktok,
     data: tiktokAccountData,
     loading,
+    error,
   } = useLinkTiktokAccount();
+
+  console.log({ tiktokAccountData, loading, error });
 
   useEffect(() => {
     const url = new URL(window.location.href);
     const authCode = url.searchParams.get('auth_code');
-    if (data?.id && authCode)
-      linkTiktok({ variables: { authCode, userProfileId: data.id } });
-  }, [data]);
+    console.log({ authCode });
+    if (data?.id && authCode) {
+      const variables = { authCode, userProfileId: data.id };
+      console.log('Linking tiktok', variables);
+      linkTiktok({ variables });
+    }
+  }, [data, linkTiktok]);
 
   useEffect(() => {
     if (!loading && tiktokAccountData) history.replace(AuthRoutes.Dashboard);
-  }, [tiktokAccountData, loading]);
+  }, [tiktokAccountData, loading, history]);
+
+  const handleModalAction = () => history.push(AuthRoutes.EditProfile);
 
   if (loading) return <FullPageLoader />;
+
   return (
-    <div className="tiktok_error_box">
-      Unable to link tiktok account, Your provided token is either invalid or
-      already used.
-    </div>
+    <Modal isOpen handleClose={handleModalAction} title="">
+      <div className="flex flex-col items-center gap-4 text-gray-600">
+        {tiktokAccountData ? (
+          <>
+            <img src="/images/checkmark-brand.svg" alt="checkmark icon" />
+            <p className="font-bold text-2xl text-center">
+              Your TikTok has been successfully connected
+            </p>
+            <Button onClick={handleModalAction}>
+              Go back to brand profile
+            </Button>
+          </>
+        ) : (
+          <>
+            <p className="font-bold text-2xl text-center text-danger">
+              There was an error linking your TikTok account. <br />
+              Please make sure you have an ads profile set up
+            </p>
+            <Button onClick={handleModalAction}>Go back to profile</Button>
+          </>
+        )}
+      </div>
+    </Modal>
   );
 };
 
