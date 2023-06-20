2492fed3f20872bc3ba889d93e4861a16b0ff30d
Adeeltahir1995 <adeeltahir1995@gmail.com>
2023-06-20 11:07:34 +0500

    chore: remove prim status filter add fields for response

--- amplify/backend/function/listAdGroups/src/tiktokApis.js
+++ amplify/backend/function/listAdGroups/src/tiktokApis.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 
 const listAdGroups = async(token, advId, campgnId) => {
     
-    const url=  process.env.TKTOK_BUSNS_API_BASE_URL+`adgroup/get/?advertiser_id=${advId}&fields=%5B%22adgroup_id%22%2C%22adgroup_name%22%5D&filtering={\"campaign_ids\":[\"${campgnId}\"],\"primary_status\": \"STATUS_DELIVERY_OK\"}`
+    const url=  process.env.TKTOK_BUSNS_API_BASE_URL+`adgroup/get/?advertiser_id=${advId}&fields=%5B%22adgroup_id%22%2C%22adgroup_name%22%2C%22secondary_status%22%2C%22operation_status%22%5D&filtering={\"campaign_ids\":[\"${campgnId}\"]}`
     const headers = { 
         'Access-Token': token
     }
